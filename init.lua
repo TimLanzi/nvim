8dedbbe0c92b7d505afc06f14c6024d7d2344110
Tim Lanzi <trlanzi@gmail.com>
2024-08-02 12:34:06 -0400

    - include custom snippets from my vscode setup

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 vim.opt.relativenumber = true
+vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/snippets"
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
